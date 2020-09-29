f16ffdb7c19890173c1bd8537c5500e8634f428d
Vivid Green <zacharycreek@gmail.com>
2020-09-29 11:54:50 -0600

    built out manager,intern,engineer and employee classes based on test scripts.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name,id,email,github) {
+        super(name,id,email,github);
+        this.github = github;
+        this.role = "Engineer"
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor(name,id,email,officeNumber) {
+        super(name,id,email,officeNumber);
+        this.officeNumber = officeNumber;
+        this.role = "Manager";
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
+
+module.exports = Manager;
