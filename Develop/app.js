1f4af83d81461b7254deb3ff1aa11fe23c60e7da
Vivid Green <zacharycreek@gmail.com>
2020-09-30 22:04:13 -0600

    Added Questions class with inquirer methods.

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Questions = require("./lib/Questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,10 +10,77 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const { resolve } = require("path");
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const getRole = () => {
+    return new Questions().inqRole();
+}
+const getAnswers = ({role} = answers) => {
+    return new Promise((resolve,reject) => {
+        new Questions().inqQuestions(role).then(answers => {
+            let obj = {
+                role: role,
+                answers: answers
+            }
+            resolve(obj);
+        }).catch(err => reject(err));
+    })
+};
+
+const addEmployee = ({role, answers} = results) => {
+    return new Promise((resolve) => {
+        answers = Object.values(answers);
+        switch (role) {
+            case "manager":
+                employees.push(new Manager(...answers));
+                break;
+            case "engineer":
+                employees.push(new Engineer(...answers));
+                break;
+            default:
+                employees.push(new Intern(...answers));
+                break;
+        }
+        resolve();
+    });
+};
+
+const addMore = () => {
+    return new Questions().inqAddMore();
+}
+
+const outputHtml = (data) => {
+    fs.writeFile(outputPath, data, (err) => {
+        if (err) throw err;
+        return console.log('The file has been saved!');
+    });
+}
+
+const runPrompt = () => {
+    // return new Promise((resolve) => {
+        getRole()
+        .then(getAnswers)
+        .then(addEmployee)
+        .then(addMore)
+        .then(({more} = answers) => {
+            // more ? runPrompt() : resolve(console.log(render(employees)));
+            if(more) {
+                runPrompt();
+            }
+            else {
+                const html = render(employees);
+                return outputHtml(html);
+            }
+        })
+    // })
+}
+
+runPrompt()
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
---
+++ Develop/lib/Questions.js
@@ -0,0 +1,172 @@
+const inquirer = require("inquirer");
+
+class Questions {
+    constructor() {
+        this.manager =  [
+            {
+                type: "input",
+                name: "name",
+                message: "What is the manager's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the office number?"
+            }
+        ];
+        this.intern = [
+            {
+                type: "input",
+                name: "name",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the name of their school?"
+            }
+        ];
+        this.engineer = [
+            {
+                type: "input",
+                name: "name",
+                message: "What is the engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their employee id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is their GitHub username?"
+            }
+        ];
+    }
+
+    inqRole() {
+        const questions = {
+            type: "list",
+            name: "role",
+            message: "What is the role of the employee?",
+            choices: ["Manager", "Intern", "Engineer"],
+            filter: function (val) {
+              return val.toLowerCase();
+            }
+        }
+        return inquirer.prompt(questions);
+    }
+
+    inqQuestions(role) {
+        let questions = this[role];
+        return inquirer.prompt(questions);
+    }
+
+    inqAddMore() {
+        const questions = {
+            type: "confirm",
+            name: "more",
+            message: "Continue adding employees?"
+        }
+        return inquirer.prompt(questions);
+    }
+}
+
+// const questions = {
+//     roles: ["Manager", "Intern", "Engineer"],
+//     manager: [
+//         //name,id,email,officeNumber
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "What is the manager's name?"
+//         },
+//         {
+//             type: "input",
+//             name: "id",
+//             message: "What is the employee id?"
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "What is their email?"
+//         },
+//         {
+//             type: "input",
+//             name: "officeNumber",
+//             message: "What is the office number?"
+//         }
+//     ],
+//     intern: [
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "What is the intern's name?"
+//         },
+//         {
+//             type: "input",
+//             name: "id",
+//             message: "What is their employee id?"
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "What is their email?"
+//         },
+//         {
+//             type: "input",
+//             name: "school",
+//             message: "What is the name of their school?"
+//         }
+//     ],
+//     engineer: [
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "What is the engineer's name?"
+//         },
+//         {
+//             type: "input",
+//             name: "id",
+//             message: "What is their employee id?"
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "What is their email?"
+//         },
+//         {
+//             type: "input",
+//             name: "github",
+//             message: "What is their GitHub username?"
+//         }
+//     ]
+// };
+
+module.exports = Questions;
